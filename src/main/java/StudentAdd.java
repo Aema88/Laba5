d85d53a2a12ef0dc0b604d19cf7ef3765f320056
Danil <doroshenkodanil88@gmail.com>
2023-12-26 13:53:46 +0200

    Первый коммит: добавление проекта

--- src/main/java/StudentAdd.java
+++ src/main/java/StudentAdd.java
@@ -7,27 +7,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 
 @WebServlet(name = "StudentAdd", urlPatterns = {"/StudentAdd"})
 public class StudentAdd extends HttpServlet {
-
-    protected void processRequest(HttpServletRequest request , HttpServletResponse response) throws ServletException, IOException{
+    private DAO studentDAO = new DAO();
+    protected void processRequest(HttpServletRequest request , HttpServletResponse response) throws ServletException, IOException, SQLException {
         HttpSession session = request.getSession();
-        List<Student> students = (List<Student>)session.getAttribute("students");
-        if(students == null){
-            students = new LinkedList<Student>();
-            session.setAttribute("students" , students);
-        }
+
         if(request.getParameter("name") != "" || request.getParameter("surname") != ""){
             Student student = new Student(request.getParameter("name"),
                                           request.getParameter("surname"),
                                           request.getParameter("email"),
                                           request.getParameter("group"),
                                           request.getParameter("faculty"));
-            students.add(student);
+            studentDAO.AddStudent(student);
         }
+        List<Student> students = new LinkedList<Student>();
+        students = studentDAO.GetStudents();
+        session.setAttribute("students" , students);
         response.sendRedirect("/student.jsp");
 
     }
@@ -35,11 +35,19 @@ public class StudentAdd extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        processRequest(req, resp);
+        try {
+            processRequest(req, resp);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        processRequest(req, resp);
+        try {
+            processRequest(req, resp);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
